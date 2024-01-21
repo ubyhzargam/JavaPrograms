17d065579e7c074717623f6eea77508aa27670aa
ubyhzargam <ubyhzargam@gmail.com>
2024-01-21 15:40:59 +0530

    update

--- Remove_duplicates_from_array_method1/RemoveDups.java
+++ Remove_duplicates_from_array_method1/RemoveDups.java
@@ -40,11 +40,15 @@ class RemoveDups
         int res=1;
         for(int i=0;i<n;i++)
         {
-            if(arr[res-1]!=arr[i])
+            if(temp[res-1]!=arr[i])
             {
-                arr[res]=arr[i];
+                temp[res]=arr[i];
                 res++;
             }
+        }
+        for(int i=0;i<res;i++)
+        {
+            arr[i]=temp[i];
         }
         return res;
     }
