a1d240a07c2cef66257352b8caf588b644037857
ubyhzargam <ubyhzargam@gmail.com>
2024-02-19 00:02:23 +0530

    19/02/24

---
+++ Delete_at_End_of_singly_linkedlist/Delete.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class Delete
+{
+    private static Node Deleteatend(Node head)
+    {
+        if(head==null||head.next==null)return null;
+        Node curr=head;
+        while(curr.next.next!=null)
+        curr=curr.next;
+        curr.next=null;
+        return head;
+    }
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the linked list");
+        int n=sc.nextInt();
+        Node head=null;
+        Node temp1=null;
+        System.out.println("Enter the elements in the linked list");
+        for(int i=0;i<n;i++)
+        {
+            int ele=sc.nextInt();
+            Node temp2=new Node(ele);
+            if(head==null)
+            {
+                head=temp1=temp2;
+                continue;
+            }
+            temp1.next=temp2;
+            temp1=temp2;
+        }
+        head=Deleteatend(head);
+        System.out.println("The modified linked list after deletion at end is given by");
+        temp1=head;
+        while(temp1!=null)
+        {
+            System.out.print(temp1.data+" ");
+            temp1=temp1.next;
+        }
+    }
+}
+class Node
+{
+    int data;
+    Node next;
+    Node(int x)
+    {
+        data=x;
+        next=null;
+    }
+}
