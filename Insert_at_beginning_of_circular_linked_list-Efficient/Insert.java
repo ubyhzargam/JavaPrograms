897e92d90ac6214c14fbbe7cc7e2e5a970afc679
ubyhzargam <ubyhzargam@gmail.com>
2024-02-29 13:20:04 +0530

    29/02/24

---
+++ Insert_at_beginning_of_circular_linked_list-Efficient/Insert.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+class Insert
+{
+    private static Node Insert(Node head, int x)
+    {
+        Node temp=new Node(x);
+        if(head==null)
+        {
+            temp.next=temp;
+            head=temp;
+            return temp;
+        }
+        temp.data=head.data;
+        temp.next=head.next;
+        head.data=x;
+        head.next=temp;
+        return head;
+    }
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the circular linked list");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements in the circular linked list");
+        Node head=null,curr=null;
+        for(int i=0;i<n;i++)
+        {
+            int ele=sc.nextInt();
+            Node temp=new Node(ele);
+            if(head==null)
+            {
+                head=curr=temp;
+                continue;
+            }
+            curr.next=temp;
+            curr=temp;
+        }
+        if(curr!=null)curr.next=head;
+        System.out.println("Enter the element you want to insert at the beginning of circular linked list");
+        int x=sc.nextInt();
+        System.out.println("The modified linked list is given by ");
+        head=Insert(head,x);
+        curr=head;
+        if(curr!=null)
+        do
+        {
+        System.out.print(curr.data+" ");
+        curr=curr.next;
+        }while(curr!=head);
+    }
+}
+class Node 
+{
+    int data;
+    Node next;
+    Node(int x)
+    {
+        data=x;
+        next=null;
+    }
+}
