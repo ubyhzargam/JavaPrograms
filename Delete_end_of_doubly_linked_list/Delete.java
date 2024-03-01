2cfc4d4f0cb706cb875333b0390ea56c1e8b08ac
ubyhzargam <ubyhzargam@gmail.com>
2024-03-01 22:52:36 +0530

    01/03/24

---
+++ Delete_end_of_doubly_linked_list/Delete.java
@@ -0,0 +1,57 @@
+ import java.util.Scanner;
+class Delete
+{
+    private static Node DeleteLast(Node head)
+    {
+        if(head==null||head.next==null)return null;
+        Node curr=head;
+        while(curr.next!=null)
+        {
+            curr=curr.next;
+        }
+        curr.prev.next=null;
+        return head;
+    }
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number of elements in the doubly linked list");
+        n=sc.nextInt();
+        System.out.println("Enter the elements in the doubly linked list");
+        Node head=null, curr=null;
+        for(int i=0;i<n;i++)
+        {
+            int ele=sc.nextInt();
+            Node temp=new Node(ele);
+            if(head==null)
+            {
+                head=curr=temp;
+                continue;
+            }
+            temp.prev=curr;
+            curr.next=temp;
+            curr=temp;
+        }
+        head=DeleteLast(head);
+        System.out.println("The modified doubly linked list after deleting the last Node is given by");
+        curr=head;
+        while(curr!=null)
+        {
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }
+        System.out.println();
+    }
+}
+class Node
+{
+    int data;
+    Node prev;
+    Node next;
+    Node(int x)
+    {
+        data=x;
+        prev=next=null;
+    }
+}
